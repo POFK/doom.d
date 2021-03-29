d44828ff55d5a03a369393adfbddc3091a7cf76b
POFK <mtianxiang@gmail.com>
2021-03-29 21:13:44 +0800

    feat(*): general configuration

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/org/gtd")))
+ '(pyim-dicts
+   (quote
+    ((:name "greatdict" :file "/opt/dict/pyim-greatdict.pyim.gz")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
