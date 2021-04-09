6cd893c42afef8711ba4d0d5cd88e1de4defcac3
POFK <mtianxiang@gmail.com>
2021-04-09 21:50:20 +0800

    feat(chinese): set F9 to auto detecte en & zh

--- modules/workflow/chinese/config.el
+++ modules/workflow/chinese/config.el
@@ -2,5 +2,9 @@
 (define-key global-map (kbd "<f6>") 'pyim-create-2cchar-word-at-point)
 (define-key global-map (kbd "<f7>") 'pyim-create-3cchar-word-at-point)
 (define-key global-map (kbd "<f8>") 'pyim-create-4cchar-word-at-point)
+(define-key global-map (kbd "<f9>") 'pyim-convert-string-at-point)
 (setq pyim-page-tooltip 'pos-tip)
 (setq pyim-backends '(pinyin-chars))
+
+(setq-default pyim-english-input-switch-functions
+              '(pyim-probe-dynamic-english))
