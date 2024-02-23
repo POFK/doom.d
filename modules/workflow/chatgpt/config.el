593cbbcbf3529e284d479061da113daafe6f65e7
TX.Mao <mtianxiang@gmail.com>
2024-02-23 08:12:48 +0800

    fix(chatgpt): fix git path

--- modules/workflow/chatgpt/config.el
+++ modules/workflow/chatgpt/config.el
@@ -1,31 +1,11 @@
 ;;; workflow/chatgpt/config.el -*- lexical-binding: t; -*-
 
-(use-package! chatgpt
-  :defer t
-  :config
-  (unless (boundp 'python-interpreter)
-    (defvaralias 'python-interpreter 'python-shell-interpreter))
-  (setq chatgpt-repo-path (expand-file-name "straight/repos/ChatGPT.el/" doom-local-dir))
-  (set-popup-rule! (regexp-quote "*ChatGPT*")
-    :side 'bottom :size .5 :ttl nil :quit t :modeline nil)
-  :bind ("C-c q" . chatgpt-query))
-
-
-(setq chatgpt-query-format-string-map '(
-                                        ;; ChatGPT.el defaults
-                                        ("doc" . "Please write the documentation for the following function.\n\n%s")
-                                        ("bug" . "There is a bug in the following function, please help me fix it.\n\n%s")
-                                        ("understand" . "What does the following function do?\n\n%s")
-                                        ("improve" . "Please improve the following code.\n\n%s")
-                                        ;; your new prompt
-                                        ("golang-gen" . "你是一个golang工程师，我会给出函数名和注释，你根据注释添加相应的内容使其完成注释中要求的功能，最后给出相应的测试程序。在生成的程序中，你要保留我添加的注释。下面我给出我的第一个需求信息： \n\n%s")))
-
 ;; accept completion from copilot and fallback to company
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
-  :bind (("C-c C-n" . 'copilot-accept-completion-by-word)
+  :bind (:map copilot-completion-map
+	 ("C-c C-n" . 'copilot-accept-completion-by-word)
          ("C-<tab>" . 'copilot-accept-completion-by-word)
-         :map copilot-completion-map
          ("<tab>" . 'copilot-accept-completion)
          ("TAB" . 'copilot-accept-completion)))
 
--- modules/workflow/chatgpt/packages.el
+++ modules/workflow/chatgpt/packages.el
@@ -1,8 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; workflow/chatgpt/packages.el
 
-(package! chatgpt
-  :recipe (:host github :repo "joshcho/ChatGPT.el" :files ("dist" "*.el")))
-
-(package! copilot
-  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+(package! copilot :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")))
